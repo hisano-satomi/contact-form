d9cef8c7190c02c1600e99e5f235bf60424d51a4
hisano satomi <stm1014@yahoo.ne.jp>
2025-05-25 11:29:18 +0900

    indexファイルにclass名を追記

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -15,49 +15,49 @@
     <div class="content">
         <form class="contact-form" action="" method="POST">
         @csrf
-            <table>
-                <tr>
-                    <th><label for="last-name">お名前<span class="contact-form--red">※</span></label></th>
-                    <td>
+            <table class="contact-table">
+                <tr class="contact-table__row">
+                    <th class="contact-table__header"><label for="last-name">お名前<span class="contact-form--red">※</span></label></th>
+                    <td class="contact-table__item">
                         <input type="text" id="last-name" name="name">
                         <input type="text" name="first-Name">
                     </td>
                 </tr>
-                <tr>
-                    <th>性別<span class="contact-form--red">※</span></th>
-                    <td>
+                <tr class="contact-table__row">
+                    <th class="contact-table__header">性別<span class="contact-form--red">※</span></th>
+                    <td class="contact-table__item">
                         <input type="radio" id="male" name="gender" value="男性"><label for="male">男性</label>
                         <input type="radio" id="female" name="gender" value="女性"><label for="female">女性</label>
                         <input type="radio" id="others" name="gender" value="女性"><label for="others">その他</label>
                     </td>
                 </tr>
-                <tr>
-                    <th><label for="email">メールアドレス<span class="contact-form--red">※</span></label></th>
-                    <td><input type="email" id="email" name="email"></td>
+                <tr class="contact-table__row">
+                    <th class="contact-table__header"><label for="email">メールアドレス<span class="contact-form--red">※</span></label></th>
+                    <td class="contact-table__item"><input type="email" id="email" name="email"></td>
                 </tr>
-                <tr>
-                    <th><label for="tel">電話番号<span class="contact-form--red">※</span></label></th>
-                    <td><input type="tel" id="tel" name="tel"></td>
+                <tr class="contact-table__row">
+                    <th class="contact-table__header"><label for="tel">電話番号<span class="contact-form--red">※</span></label></th>
+                    <td class="contact-table__item"><input type="tel" id="tel" name="tel"></td>
                 </tr>
-                <tr>
-                    <th><label for="address">住所<span class="contact-form--red">※</span></label></th>
-                    <td><input type="text" id="address" name="address"></td>
+                <tr class="contact-table__row">
+                    <th class="contact-table__header"><label for="address">住所<span class="contact-form--red">※</span></label></th>
+                    <td class="contact-table__item"><input type="text" id="address" name="address"></td>
                 </tr>
-                <tr>
-                    <th><label for="building">建物名</label></th>
-                    <td><input type="text" id="building" name="building"></td>
+                <tr class="contact-table__row">
+                    <th class="contact-table__header"><label for="building">建物名</label></th>
+                    <td class="contact-table__item"><input type="text" id="building" name="building"></td>
                 </tr>
-                <tr>
-                    <th><label for="category">お問い合わせの種類<span class="contact-form--red">※</span></label></th>
-                    <td>
+                <tr class="contact-table__row">
+                    <th class="contact-table__header"><label for="category">お問い合わせの種類<span class="contact-form--red">※</span></label></th>
+                    <td class="contact-table__item">
                         <select id="category" name="category">
                             <option value="">選択してください</option>
                         </select>
                     </td>
                 </tr>
-                <tr>
-                    <th><label for="content">お問い合わせ内容<span class="contact-form--red">※</span></label></th>
-                    <td><textarea id="content" name="content" rows="5"></textarea></td>
+                <tr class="contact-table__row">
+                    <th class="contact-table__header"><label for="content">お問い合わせ内容<span class="contact-form--red">※</span></label></th>
+                    <td class="contact-table__item"><textarea id="content" name="content" rows="5"></textarea></td>
                 </tr>
             </table>
             <button type="submit">確認画面</button>
