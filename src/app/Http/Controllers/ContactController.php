e8cccfac3268117b0fef0ff6076eb901f5252a26
hisano satomi <stm1014@yahoo.ne.jp>
2025-05-26 22:08:35 +0900

    完了ページのスタイル作成

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -8,7 +8,7 @@ class ContactController extends Controller
 {
     public function index()
     {
-        return view('confirm');
+        return view('thanks');
     }
     
 }
--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('thanks.css') }}">
+<link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
 @endsection
 
 @section('content')
-<div class="content">
-    <div>
+<div class="thanks-bg-text">Thank you</div>
+<div class="thanks-content">
+    <div class="thanks-content__text">
         お問い合わせありがとうございました
     </div>
-    <button type="button">HOEM</button>
+    <div class="thanks-content__button">
+        <a class="thanks-content__link" href="/">HOME</a>
+    </div>
 </div>
 @endsection
