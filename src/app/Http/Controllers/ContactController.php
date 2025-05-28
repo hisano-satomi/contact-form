c891a5e618b6f0a37a4a68561bcf556ce594bcfb
hisano satomi <stm1014@yahoo.ne.jp>
2025-05-28 16:34:50 +0900

    バリデーションの作成

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -24,7 +24,31 @@ class ContactRequest extends FormRequest
     public function rules()
     {
         return [
-            
+            'category_id' => ['required'],
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'gender' => ['required'],
+            'email' => ['required', 'email'],
+            'tel' => ['required', 'digits_between:1,5', 'regex:/^[0-9]+$/'],
+            'address' => ['required'],
+            'detail' => ['required', 'max:120'],
         ];
     }
-}
+
+    public function messages()
+    {
+        return [
+            'category_id.required' => 'お問い合わせの種類を選択してください',
+            'first_name.required' => '姓を入力してください',
+            'last_name.required' => '名を入力してください',
+            'gender.required' => '性別を選択してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'tel.required' => '電話番号を入力してください',
+            'tel.digits_between' => '電話番号は5桁までの数字で入力してください';
+            'tel.regex' => '電話番号は5桁までの数字で入力してください',
+            'address.required' => '住所を入力してください',
+            'detail.required' => 'お問い合わせ内容を入力してください',
+            'detail.max' => 'お問い合わせ内容は120文字以内で入力してください',
+        ]
+    }
