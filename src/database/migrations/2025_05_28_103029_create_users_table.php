8280deeaaac7728e8ea517069d130593b1c345b0
hisano satomi <stm1014@yahoo.ne.jp>
2025-05-28 10:54:13 +0900

    マイグレーションファイルの編集とデザインフォルダの作成

--- src/database/migrations/2025_05_28_103029_create_users_table.php
+++ src/database/migrations/2025_05_28_103029_create_users_table.php
@@ -15,6 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->string('email', 255);
+            $table->string('password', 255);
             $table->timestamps();
         });
     }
