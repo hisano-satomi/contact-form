d928a2f3e92aad79ce4b9914c9ef820c2bc8fe29
hisano satomi <stm1014@yahoo.ne.jp>
2025-06-04 22:36:34 +0900

    シード実行時のエラーを修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // 外部キー制約を一時的に無効化
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
         $this->call([
             CategorySeeder::class,
+            ContactsTableSeeder::class,
         ]);
+
+        // 外部キー制約を有効に戻す
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
